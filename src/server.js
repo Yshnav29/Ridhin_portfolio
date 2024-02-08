1bb38ec1948b61eac2c84cba5b47f58d57c467d1
Yshnav29 <vyshnavard@gmail.com>
2024-02-08 22:32:37 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import path from 'path';
+import { config } from 'dotenv';
+config({path: '../email.env'});
+import { createTransport } from 'nodemailer';
+
+var app = express();
+
+var port = 4000
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("../public/"));
+
+
+app.post("/send_email", function(req, response) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var subject = req.body.subject;
+    var message = req.body.comment;
+
+    var transporter = createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+            // 'rpcjmkwhwcyhauev'
+        }
+    });
+
+    var mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: 'vyshnavard@gmail.com',
+        subject: subject,
+        text: `Name: ${name}\n E-mail: ${email}\n Message: ${message}`,
+        html: `<h2>Name: ${name} <br> E-mail: ${email}<br> Message: ${message}</h2>`
+    }
+
+    transporter.sendMail(mailOptions, (error,info) => {
+        if(error) {
+            console.log(error);
+        }
+        else {
+            console.log("Email send: " + info.response);
+            alert("Form was successfully submiteed.");
+        }
+        response.redirect("/");
+    })
+})
+
+app.listen(4000, () => {
+    console.log(`Server is running on port ${port}`);
+})
